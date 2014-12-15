6a34877a3a7ab9345f0e1932a7cfbcd5fde34a9f
Kelly Selden <kellyselden@gmail.com>
2014-12-14 21:30:17 -0500

    added custom mongodb server

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,80 +1,66 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var fs = require('fs');
 
-mongoose.connect('mongodb://localhost/app');
+var connectionstring = fs.readFileSync('connectionstring', 'utf8');
+
+mongoose.connect(connectionstring);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-var Product = new mongoose.Schema({
+var ProductSchema = new mongoose.Schema({
   name: { type: String, required: true }
 });
-var ProductModel = mongoose.model('Product', Product);  
+var ProductModel = mongoose.model('Product', ProductSchema);  
 
-router.get('/', function (req, res) {
-  return ProductModel.find(function (err, products) {
+router.get('/', function(req, res) {
+  return ProductModel.find(function(err, products) {
     if (err) return console.error(err);
     return res.send({
       products: products
     });
-    console.log(products);
   });
 });
 
-router.post('/', function (req, res){
-  var product;
-  console.log("POST: ");
-  console.log(req.body);
-  product = new ProductModel({
+router.post('/', function(req, res) {
+  var product = new ProductModel({
     name: req.body.product.name
   });
-  product.save(function (err) {
-    if (!err) {
-      return console.log("created");
-    } else {
-      return console.log(err);
-    }
-  });
-  return res.send(product);
-});
-
-router.get('/:id', function (req, res){
-  return ProductModel.findById(req.params.id, function (err, product) {
-    if (!err) {
-      return res.send({
-        product: product
-      });
-    } else {
-      return console.log(err);
-    }
-  });
-});
-
-router.put('/:id', function (req, res){
-  return ProductModel.findById(req.params.id, function (err, product) {
-    product.title = req.body.title;
-    product.description = req.body.description;
-    product.style = req.body.style;
-    return product.save(function (err) {
-      if (!err) {
-        console.log("updated");
-      } else {
-        console.log(err);
-      }
-      return res.send(product);
+  return product.save(function(err) {
+    if (err) console.error(err);
+    return res.send({
+      product: product
     });
   });
 });
 
-router.delete('/:id', function (req, res){
-  return ProductModel.findById(req.params.id, function (err, product) {
-    return product.remove(function (err) {
-      if (!err) {
-        console.log("removed");
-        return res.send('');
-      } else {
-        console.log(err);
-      }
+router.get('/:id', function(req, res) {
+  return ProductModel.findById(req.params.id, function(err, product) {
+    if (err) return console.error(err);
+    return res.send({
+      product: product
+    });
+  });
+});
+
+router.put('/:id', function(req, res) {
+  return ProductModel.findById(req.params.id, function(err, product) {
+    product.name = req.body.product.name;
+    return product.save(function(err) {
+      if (err) console.error(err);
+      return res.send({
+        product: product
+      });
+    });
+  });
+});
+
+router.delete('/:id', function(req, res) {
+  return ProductModel.findById(req.params.id, function(err, product) {
+    return product.remove(function(err) {
+      if (err) console.error(err);
+      return res.send('');
     });
   });
 });
