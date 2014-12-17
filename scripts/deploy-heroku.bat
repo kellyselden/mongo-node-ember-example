79d9e4547bcac688118fd52cbc0f0b081c2098cc
Kelly Selden <kellyselden@gmail.com>
2014-12-16 21:22:21 -0500

    don't check in server name

--- scripts/deploy-heroku.bat
+++ scripts/deploy-heroku.bat
@@ -1,12 +1,13 @@
 pushd %~dp0
 call build
+for /f %%i in (heroku) do set heroku=%%i
 pushd ..\server
 md heroku
 ROBOCOPY /MIR . heroku /XD node_modules heroku /XF .gitignore
 pushd heroku
 git init
 git add . && git commit -a -m "deploy"
-call heroku git:remote -a gentle-crag-3506
+call heroku git:remote -a %heroku%
 git push heroku master -f
 popd
 popd
