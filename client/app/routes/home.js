4dbcc7ae75aa0004f89135e1d481e98ed043c004
Kelly Selden <kellyselden@gmail.com>
2014-12-30 19:35:24 -0500

    adding ember-cli-transition-to-dynamic

--- client/app/routes/home.js
+++ client/app/routes/home.js
@@ -6,21 +6,8 @@ export default Ember.Route.extend({
   },
   actions: {
     localeChanged: function(locale) {
-      var routes = this.router.router.currentHandlerInfos;
-      var models = [];
-      for (var i = 0; i < routes.length; i++) {
-        var params = routes[i].params;
-        for (var param in params) {
-          if (params.hasOwnProperty(param)) {
-            models.push(param === 'locale' ? locale : params[param]);
-          }
-        }
-      }
-
-      var args = models.slice();
       var currentRouteName = this.controllerFor('application').get('currentRouteName');
-      args.unshift(currentRouteName);
-      this.transitionTo.apply(this, args);
+      this.transitionToDynamic(currentRouteName, { locale: locale });
     }
   }
 });
