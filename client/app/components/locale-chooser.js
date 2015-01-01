83b82cf35776f5b68d0c07fc0fc1b8f36c7a10aa
Kelly Selden <kellyselden@gmail.com>
2014-12-31 19:45:15 -0500

    added ember-cli-i18n-observer

--- client/app/components/locale-chooser.js
+++ client/app/components/locale-chooser.js
@@ -5,8 +5,7 @@ var ignoreSelectedLocaleChanged;
 
 export default Ember.Component.extend({
   localeSetup: function() {
-    var application = this.container.lookup('application:main');
-    var currentLocale = application.get('locale');
+    var currentLocale = this.get('i18n.locale');
     this.set('locales', locales.map(function(locale) {
       var option = { value: locale, label: this.t('locales.' + locale) };
       if (locale === currentLocale) {
@@ -15,7 +14,7 @@ export default Ember.Component.extend({
       }
       return option;
     }.bind(this)));
-  }.on('init'),
+  }.observes('i18n.locale').on('init'),
   selectedLocaleChanged: function() {
     if (ignoreSelectedLocaleChanged) {
       ignoreSelectedLocaleChanged = false;
