c621599319c6ef73b2e8fdd352ee1699a34cde56
Kelly Selden <kellyselden@gmail.com>
2015-01-02 17:48:32 -0500

    fixing the menu activation

--- client/app/routes/home.js
+++ client/app/routes/home.js
@@ -5,6 +5,9 @@ export default Ember.Route.extend({
     this.container.lookup('application:main').set('locale', params.locale);
   },
   actions: {
+    changeMenu: function(routeName) {
+      this.controller.set('menuRoute', routeName);
+    },
     localeChanged: function(locale) {
       var currentRouteName = this.controllerFor('application').get('currentRouteName');
       this.transitionToDynamic(currentRouteName, { home: { locale: locale } });
