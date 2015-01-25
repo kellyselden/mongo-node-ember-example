975c5a0507435a2ebe7318449607cc434d3425f0
Kelly Selden <kellyselden@gmail.com>
2015-01-25 09:49:28 -0500

    error handling script

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,6 +1,19 @@
 pushd %~dp0..\client
+set count=0
+:restart
 call npm prune
 call npm install
 call bower prune
-call bower install --config.interactive=false
+call bower install --config.interactive=false 2>stderr.txt
+type stderr.txt
+findstr "ECONFLICT" stderr.txt
+if %errorlevel%==0 goto :error
+goto :success
+:error
+if %count% geq 10 exit /b 1
+set /a count+=1
+rd bower_components /s /q
+goto :restart
+:success
+del stderr.txt
 popd
