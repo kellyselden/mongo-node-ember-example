185a97ccd7adc29095cec360d51ebbe2f040f7a8
Kelly Selden <kellyselden@gmail.com>
2015-02-04 20:28:04 -0500

    prepping for submodule

--- scripts/clean-tmp.bat
+++
@@ -1,3 +0,0 @@
-pushd %~dp0..\client
-for /d %%p in ("tmp\*") do rd "%%p" /s /q
-popd
--- scripts/debug-client-against-heroku.bat
+++
@@ -1,8 +0,0 @@
-pushd %~dp0
-for /f %%i in (heroku) do set heroku=%%i
-pushd ..\client
-<nul set /p=http://%heroku%.herokuapp.com>host
-call ember s --environment=heroku
-del /q host
-popd
-popd
