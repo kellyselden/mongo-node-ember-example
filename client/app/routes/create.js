1a776274cec82d6341b793a2dc4bf0db71328c25
Kelly Selden <kellyselden@gmail.com>
2015-02-07 10:11:10 -0500

    adding dirty checking to the create route

--- client/app/routes/create.js
+++ client/app/routes/create.js
@@ -1,15 +1,10 @@
 import Ember from 'ember';
+import DirtyConfirmRouteMixin from 'ember-cli-dirty-confirm/mixins/dirty-confirm-route';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(DirtyConfirmRouteMixin, {
   model: function(type, hash) {
     return Ember.RSVP.hash(hash || {}).then(function(hash) {
       return this.store.createRecord(type, hash);
     }.bind(this));
-  },
-  deactivate: function() {
-    var model = this.currentModel;
-    if (model.get('isDirty') && !model.get('isSaving')) {
-      this.store.unloadRecord(model);
-    }
   }
 });
