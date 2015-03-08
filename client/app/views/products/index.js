3e34431b6002c36a4976c32196654e135664a7f3
Kelly Selden <kellyselden@gmail.com>
2015-03-07 22:29:44 -0500

    slide animation update

--- client/app/views/products/index.js
+++ client/app/views/products/index.js
@@ -1,4 +1,4 @@
 import Ember from 'ember';
-import SlideViewMixin from 'client/mixins/slide-view';
+import SlideViewMixin from 'ember-cli-slide-animation/mixins/slide-view';
 
 export default Ember.View.extend(SlideViewMixin);
