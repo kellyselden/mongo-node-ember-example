a45086726a5c574c241e1f840beb3a0f8ec9de5a
Kelly Selden <kellyselden@gmail.com>
2015-03-08 12:48:51 -0400

    error stack workaround

--- client/app/controllers/error.js
+++ client/app/controllers/error.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  // error.stack has some weird issues, this works only the first time
+  stack: function() {
+    return this.get('error').stack;
+  }.property('error.stack')
 });
