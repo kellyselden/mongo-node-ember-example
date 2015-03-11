13056d1cc2a356895496226c00146609c978c1c3
Kelly Selden <kellyselden@gmail.com>
2015-03-11 12:14:58 -0400

    fixing the stacking of confirms

--- client/app/initializers/version.js
+++ client/app/initializers/version.js
@@ -1,10 +1,22 @@
+var secondsBetweenConfirms = 10;
+
 export function initialize(container, application) {
   var socket = container.lookup('io:main');
 
+  // prevent messages from stacking up
+  var lastRunTime = moment();
+
   socket.on('version', function(msg) {
     var isNewVersion = msg !== application.version;
-    if (isNewVersion && confirm('App updated. Reload?')) {
-      window.location.reload();
+    if (isNewVersion) {
+      var secondsSinceLastRunTime = moment().subtract(lastRunTime).seconds();
+      var hasEnoughTimePassed = secondsSinceLastRunTime > secondsBetweenConfirms;
+      if (hasEnoughTimePassed) {
+        if (confirm('App updated. Reload?')) {
+          window.location.reload();
+        }
+        lastRunTime = moment();
+      }
     }
   });
 }
