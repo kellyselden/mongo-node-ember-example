6ad9e3747d9fb3086e4d6fed55228f1b4420a373
Kelly Selden <kellyselden@gmail.com>
2015-03-14 10:17:02 -0400

    adding ember-admin

--- client/app/router.js
+++ client/app/router.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 import config from './config/environment';
+import adminRouter from 'ember-admin/router';
 
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
+  adminRouter(this);
   this.resource('home', { path: ':locale' }, function() {
     this.resource('products', function() {
       this.route('create');
