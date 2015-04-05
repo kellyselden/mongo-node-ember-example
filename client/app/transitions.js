fa839adb0daa1ad9db95f56c80128bd59406d747
Kelly Selden <kellyselden@gmail.com>
2015-04-05 17:47:36 -0400

    adding fallback transition

--- client/app/transitions.js
+++ client/app/transitions.js
@@ -1,26 +1,22 @@
 export default function() {
   this.transition(
-    this.fromRoute('home.index'),
-    this.toRoute('products.index'),
-    this.use('toLeft'),
-    this.reverse('toRight')
-  );
-  this.transition(
     this.fromRoute('products.index'),
     this.toRoute('products.product'),
-    // this.use('toLeft'),
     this.use('explode', {
       matchBy: 'product-id',
       use: 'flyTo'
     }, {
       use: 'toLeft'
     }),
-    // this.reverse('toRight')
     this.reverse('explode', {
       matchBy: 'product-id',
       use: 'flyTo'
     }, {
       use: 'toRight'
     })
+  );
+  this.transition(
+    this.use('toLeft'),
+    this.reverse('toRight')
   );
 }
