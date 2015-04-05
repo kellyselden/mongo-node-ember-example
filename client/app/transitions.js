3855c77d5213acd15f9039d25d583a2dbb29cb6e
Kelly Selden <kellyselden@gmail.com>
2015-04-05 17:41:48 -0400

    testing out hero transition

--- client/app/transitions.js
+++ client/app/transitions.js
@@ -8,7 +8,19 @@ export default function() {
   this.transition(
     this.fromRoute('products.index'),
     this.toRoute('products.product'),
-    this.use('toLeft'),
-    this.reverse('toRight')
+    // this.use('toLeft'),
+    this.use('explode', {
+      matchBy: 'product-id',
+      use: 'flyTo'
+    }, {
+      use: 'toLeft'
+    }),
+    // this.reverse('toRight')
+    this.reverse('explode', {
+      matchBy: 'product-id',
+      use: 'flyTo'
+    }, {
+      use: 'toRight'
+    })
   );
 }
