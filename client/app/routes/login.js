ed4188ad8a3623e9e2e0b72d3e8c29477e9554ff
Kelly Selden <kellyselden@gmail.com>
2015-04-10 17:09:43 -0400

    more torii stuff

--- client/app/routes/login.js
+++ client/app/routes/login.js
@@ -4,7 +4,9 @@ export default Ember.Route.extend({
   actions: {
     github: function() {
       this.get('session').open('github-oauth2').then(function() {
-        
+        console.log(arguments);
+      }.bind(this), function(error) {
+        this.controller.set('error', 'Could not sign you in: ' + error.message);
       }.bind(this));
     }
   }
--- server/router.js
+++ server/router.js
@@ -5,4 +5,12 @@ var products = require('./routes/products');
 
 router.use('/products', products);
 
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username);
+  });
+
 module.exports = router;
