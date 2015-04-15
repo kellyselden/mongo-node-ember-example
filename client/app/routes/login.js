4e49d53a0d5149680fe939d9252bf24cd103ccc4
Kelly Selden <kellyselden@gmail.com>
2015-04-14 21:56:08 -0400

    deprecation fix

--- client/app/routes/login.js
+++ client/app/routes/login.js
@@ -5,9 +5,9 @@ export default Ember.Route.extend({
     github: function() {
       this.get('session').open('github-oauth2').then(function() {
         console.log(arguments);
-      }.bind(this), function(error) {
-        this.controller.set('error', 'Could not sign you in: ' + error.message);
-      }.bind(this));
+      }, function() {
+        console.log(arguments);
+      });
     }
   }
 });
